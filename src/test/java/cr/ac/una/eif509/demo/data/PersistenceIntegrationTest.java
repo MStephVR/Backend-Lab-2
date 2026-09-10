d709b7e961cde211f683105c7d106c8726ec0094
MStephVR <MStephVR@users.noreply.github.com>
2026-09-10 14:38:39 -0600

    Improve integration test CI diagnostics

--- src/test/java/cr/ac/una/eif509/demo/data/PersistenceIntegrationTest.java
+++ src/test/java/cr/ac/una/eif509/demo/data/PersistenceIntegrationTest.java
@@ -42,7 +42,8 @@ class PersistenceIntegrationTest {
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
         registry.add("spring.datasource.driver-class-name", POSTGRES::getDriverClassName);
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "validate");
+                registry.add("spring.jpa.hibernate.ddl-auto", () -> "validate");
+                registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
         registry.add("spring.flyway.enabled", () -> "true");
     }
 
